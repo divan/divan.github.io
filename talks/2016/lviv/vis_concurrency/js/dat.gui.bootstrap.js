6379d56ae698842283c2028d9ba317d4464d6a37
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-10-19 10:11:53 +0100

    Remove talks from this repo

--- talks/2016/lviv/vis_concurrency/js/dat.gui.bootstrap.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-  var gui = new dat.GUI();
-  var pauseController = gui.add(sample_defaults, "paused");
-  var wireframeController = gui.add(sample_defaults, "wireframe");
-
-  gui.closed = true;
-});
